00206f3fbe641a8cdc0295dbaf039776806ce89f
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2021-04-04 17:57:20 +0500

    Create Driver.java
    Added the first print statement in it.
    The purpose of this change is to try commit change in the repository.

---
+++ procurementSystem/src/procurementsystem/Driver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this tempslate file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procurementsystem;
+
+/**
+ *
+ * @author M_Khuzaima
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is the procurement Management System!");
+    }
+    
+}

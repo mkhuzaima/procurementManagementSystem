5b5f0cee6115fd198098c70908975fc45036509b
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2021-04-15 22:33:54 +0500

    converted to NetBeans Project

---
+++ PMS/src/pms/Driver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this tempslate file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pms;
+
+/**
+ *
+ * @author M_Khuzaima
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LoginForm frame = new LoginForm();
+        frame.setVisible(true);
+    }
+    
+}
---
+++ PMS/src/pms/Manager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pms;
+
+/**
+ * This class is used to store and manipulate data of the manager.
+ * @author M_Khuzaima
+ */
+public class Manager extends Person {
+    
+}
